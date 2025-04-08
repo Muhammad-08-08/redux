1ef2c1049bb200ecb8b990c3ebfbdc8c26676daf
Muhammad-Nuraliyev <muhammad.nuraliyev208@gmail.com>
2025-04-08 13:52:28 +0500

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
+import { Button, Form, Input } from "antd";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { decrement, increment, store } from "./store/redux";
+import {
+  add,
+  completed,
+  deleted,
+  inputValue,
+  RootState,
+  store,
+} from "./store/redux";
 
 function App() {
   return (
     <Provider store={store}>
-      <OnCounter />
+      <TodoList />
     </Provider>
   );
 }
 
-function OnCounter() {
-  const count = useSelector((state: any) => state.counter.value);
+function TodoList() {
+  const todo = useSelector((state: RootState) => state.todo.todo);
+  const input = useSelector((state: RootState) => state.todo.input);
   const dispatch = useDispatch();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Count: {count}</h1>
-      <div className="flex gap-4">
-        <button
-          className="px-6 py-3 text-xl font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <button
-          className="px-6 py-3 text-xl font-semibold text-white bg-red-500 rounded-lg shadow-md hover:bg-red-600 transition duration-300"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-      </div>
+    <div className="w-[700px] mx-auto bg-slate-800 text-white my-10 p-6 rounded-2xl">
+      <Form className="flex gap-3">
+        <Form.Item className="w-full">
+          <Input
+            value={input}
+            onChange={(e) => {
+              dispatch(inputValue(e.currentTarget.value));
+            }}
+            placeholder="todo qo'shish"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            onClick={() => {
+              dispatch(add(input));
+            }}
+            htmlType="submit"
+            type="primary"
+          >
+            Add
+          </Button>
+        </Form.Item>
+      </Form>
+      {todo.map((item) => {
+        return (
+          <div className="flex justify-between items-center">
+            <p
+              className={
+                item.completed
+                  ? "line-through text-green-600 mb-4"
+                  : "text-white mb-4"
+              }
+            >
+              {item.name}
+            </p>
+            <div className="flex gap-2">
+              <Button
+                onClick={() => {
+                  dispatch(completed(item.id));
+                }}
+              >
+                comp
+              </Button>
+              <Button
+                onClick={() => {
+                  dispatch(deleted(item.id));
+                }}
+              >
+                delet
+              </Button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/store/redux.ts
+++ src/store/redux.ts
@@ -4,6 +4,15 @@ interface counterSliceType {
   value: number;
 }
 
+interface todoSliceType {
+  todo: {
+    id: number;
+    name: string;
+    completed: boolean;
+  }[];
+  input: string;
+}
+
 const counterSlice = createSlice({
   name: "counter",
   initialState: { value: 0 } as counterSliceType,
@@ -17,8 +26,48 @@ const counterSlice = createSlice({
   },
 });
 
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    todo: [
+      {
+        id: 1,
+        name: "Todo 1",
+        completed: true,
+      },
+    ],
+    input: "",
+  } as todoSliceType,
+  reducers: {
+    add: (state, { payload }) => {
+      state.todo.push({
+        id: Math.floor(Math.random() * 10000),
+        name: payload,
+        completed: false,
+      });
+    },
+    completed: (state, { payload }) => {
+      const todoCompleted = state.todo.find((item) => item.id === payload);
+      if (todoCompleted) {
+        todoCompleted.completed = !todoCompleted.completed;
+      }
+    },
+    deleted: (state, { payload }) => {
+      state.todo = state.todo.filter((item) => item.id !== payload);
+    },
+
+    inputValue: (state, { payload }) => {
+      state.input = payload;
+    },
+  },
+});
+
 export const { increment, decrement } = counterSlice.actions;
+export const { add, deleted, completed, inputValue } = todoSlice.actions;
 
 export const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: { counter: counterSlice.reducer, todo: todoSlice.reducer },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
